4fa708e4bc07bd487308b54a7bd3085f6166bc9b
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2024-05-18 17:54:46 +0530

    Add files via upload

--- src/person.js
+++ src/person.js
@@ -1,17 +1,23 @@
-
+var a = 123;
 class Person {
-    // Class members
-    // Class methods
-
-    // First function to execute in a class
-    // Called by default when we create obj for class
-    constructor(name){
-        this.personName = name
-    }
-    walk(){
-        console.log(`Person Name is ${this.personName}`)
-    }
+  constructor(name) {
+    this.personName = name;
+    let pName = name;
+  }
+  listen() {
+    // class methods
+    console.log(`${this.personName} is listening to lecture `);
+  }
+  walk() {
+    console.log(this.personName);
+  }
 }
-export default Person;
 
-    
+export var a;
+export const key = "SWERA";
+export var obj1 = {
+  name: "XXX",
+  age: 20,
+};
+export var arr = [1, 2, 3, 4, 5];
+export default Person;
